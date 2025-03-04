af7c3a3a83988f3d973c2e81000f985297e88b9c
NBAS-5535 <uras@yahoo.com>
2025-03-04 14:23:09 -0600

    actuator command testing 2

--- src/main/java/frc/robot/commands/ActuatorCommand.java
+++ src/main/java/frc/robot/commands/ActuatorCommand.java
@@ -26,7 +26,7 @@ public class ActuatorCommand extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    //m_actuator.markPosition();
+    m_actuator.markPosition();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
